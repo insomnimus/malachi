20d71f9c8ff06f874d67ad84b06b0e5c9aaaeef5
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-10-28 21:34:37 +0300

    test: added some tests for the compiler

---
+++ src/compiler/tests.rs
@@ -0,0 +1,19 @@
+use super::Command;
+
+#[test]
+fn test_compile() {
+	let tests = &[
+		r".bet <amount>",
+		r".bible
+[
+	<book?: starts(`book=`)>
+	<chapter?: starts(`chapter=`); starts(`chap=`)>
+	<verse?: starts(`verse=`)>
+]",
+		r"no capture here!",
+		r"<maybe-prefix?> bar",
+	];
+	for s in tests {
+		Command::new(s).unwrap();
+	}
+}
